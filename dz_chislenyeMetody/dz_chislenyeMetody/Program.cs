8c6828f3b5ae962005cc40fb8356423d13a64d42
Certer001 <76663981+Certer001@users.noreply.github.com>
2023-01-22 22:05:33 +0300

    homework №1

---
+++ dz_chislenyeMetody/dz_chislenyeMetody/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = Console.Read();
+int b = Console.Read();
+int c = Console.Read();
+int d = Console.Read();
+
+double result;
+
+if (a < 0)
+{
+    result = (a + b + c) / d;
+}
+else if (a == 0)
+{
+    result = 100 + b * c - d;
+}
+else if (a > 0)
+{
+    result = a * b * c * d;
+}
+else
+{
+    result = a * b;
+}
+
+Console.WriteLine(result);
