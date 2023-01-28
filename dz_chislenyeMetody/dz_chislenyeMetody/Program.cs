3df83f7e1200f090f06ae9596df734d3ac281e46
Certer001 <76663981+Certer001@users.noreply.github.com>
2023-01-29 01:01:44 +0300

    praktika

--- dz_chislenyeMetody/dz_chislenyeMetody/Program.cs
+++ dz_chislenyeMetody/dz_chislenyeMetody/Program.cs
@@ -1,25 +1,58 @@
-﻿int a = Console.Read();
-int b = Console.Read();
-int c = Console.Read();
-int d = Console.Read();
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-double result;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
 
-if (a < 0)
-{
-    result = (a + b + c) / d;
-}
-else if (a == 0)
-{
-    result = 100 + b * c - d;
-}
-else if (a > 0)
-{
-    result = a * b * c * d;
-}
-else
-{
-    result = a * b;
-}
+        repit:
 
-Console.WriteLine(result);
+            int d = Convert.ToInt32(Console.ReadLine());
+
+            double result = 0;
+
+            if (a < 0)
+            {
+                while (true)
+                {
+                    if (d == 0)
+                    {
+                        Console.WriteLine("попытка деления на нуль");
+                        Console.WriteLine("повтори ещё раз");
+                        goto repit;
+                    }
+                    else
+                    {
+                        result = (a + b + c) / d;
+                        break;
+                    }
+                }
+
+            }
+            else if (a == 0)
+            {
+                result = 100 + b * c - d;
+            }
+            else if (a > 0)
+            {
+                result = a * b * c * d;
+            }
+            else
+            {
+                result = a * b;
+            }
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
